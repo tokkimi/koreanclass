f4c7f48880a9c700e197705ee1902d947ae3bbcb
tokkimi <goocciland@gmail.com>
2026-09-27 03:17:16 +0900

    Use complete mobile door panels under glass header

--- src/components/ScrollHero.tsx
+++ src/components/ScrollHero.tsx
@@ -181,9 +181,10 @@ export function ScrollHero({
           )}
         </div>
         <div className="scroll-hero__veil" aria-hidden="true" />
-        <div className="scroll-hero__hanok" aria-hidden="true">
-          <div className="scroll-hero__door scroll-hero__door--left"><i /><i /><i /></div>
-          <div className="scroll-hero__door scroll-hero__door--right"><i /><i /><i /></div>
+          <div className="scroll-hero__hanok" aria-hidden="true">
+            <div className="scroll-hero__door scroll-hero__door--left"><i /><i /><i /></div>
+            <div className="scroll-hero__door scroll-hero__door--center"><i /><i /><i /></div>
+            <div className="scroll-hero__door scroll-hero__door--right"><i /><i /><i /></div>
           <div className="scroll-hero__door-top"><span>서울</span></div>
           <div className="scroll-hero__door-threshold" />
         </div>
